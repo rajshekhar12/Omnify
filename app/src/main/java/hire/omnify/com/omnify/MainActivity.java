bfeae842d693054acc4c1f22fc1a13dae6a48691
rajshekhar12 <rajshekhar.cs@hotmail.com>
2018-01-31 11:35:13 +0530

    intial commit

---
+++ app/src/main/java/hire/omnify/com/omnify/MainActivity.java
@@ -0,0 +1,18 @@
+package hire.omnify.com.omnify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout gFrameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
